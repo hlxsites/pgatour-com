1369616f01f35836e41cacbb3ea15a0ec835aaea
Sean Steimer <ssteimer@adobe.com>
2022-12-08 15:37:18 -0800

    test: ejs

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,3 +1,55 @@
+function getTemplate() {
+  return `
+  <div class="footer-container">
+    <div class="brand">
+      <a href="<%= logo.href; %>" target="_blank" rel="noopener">
+        <img src="<%= logo.src; %>" alt="<%= logo.alt; %>">
+      </a>
+    </div>
+    <nav>
+      <ul class="nav-lists">
+        <% navLists.forEach((navList) => { %>
+        <li class="nav-list">
+          <span class="nav-list-heading"><%= navList.heading; %></span>
+          <ul class="nav-sublist">
+          <% navList.items.forEach((navItem) => { %>
+            <li class="nav-item">
+              <a href="<%= navItem.href; %>" target="_blank" rel="noopener">
+                <%- navItem.image; %>
+                <%= navItem.name; %>
+              </a>
+            </li>
+          <% }); %>
+          </ul>
+        </li>
+        <% }); %>
+        <li class="nav-list nav-privacy">
+          <span class="nav-list-heading">Privacy & Use</span>
+          <ul class="nav-sublist">
+            <% privacyLinks.forEach((privacyLink) => { %>
+            <li class="nav-item">
+              <a href="<%= privacyLink.href; %>" target="_blank" rel="noopener"><%= privacyLink.name; %></a>
+            </li>
+            <% }); %>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <div class="social-links">
+      <p class="copyright"><%= copyrightText; %></p>
+      <div class="social-container">
+        <% socialLinks.forEach((link) => { %>
+        <a target="_blank" rel="noopener" href="<%= link.href; %>">
+          <%- link.icon; %>
+        </a>
+        <% }); %>
+      </div>
+    </div>
+    <p class="legal"><%= legalText; %></p>
+  </div>
+  `;
+}
+
 function decorateLogo(logo, container) {
   const logoDiv = document.createElement('div');
   logoDiv.classList.add('brand');
@@ -98,7 +150,53 @@ function decorateLegal(trademarksText, container) {
   container.append(legal);
 }
 
-export default async function decorate(block) {
+export default async function decorateEjs(block) {
+  const logo = block.querySelector('a > img');
+  const navLists = block.querySelectorAll('nav h3 + ul');
+  const trademarksText = block.querySelector(':scope > p');
+  const copyRightText = block.querySelector(':scope > div > p');
+  const socialLinks = copyRightText.closest('div').querySelectorAll('a');
+  const privacyLinks = block.querySelectorAll(':scope > div > a');
+
+  const data = {
+    logo: {
+      href: logo.closest('a').href,
+      alt: logo.alt,
+      src: logo.src,
+    },
+    navLists: [...navLists].map((navList) => {
+      const parent = navList.closest('div');
+      const navHeader = parent.querySelector('h3').innerText;
+      const navLinks = [...navList.querySelectorAll('li')].map((item) => {
+        const a = item.querySelector('a');
+        const linkImg = a.querySelector('img');
+        return {
+          href: a.href,
+          name: a.innerText,
+          image: linkImg ? linkImg.outerHTML : '',
+        };
+      });
+      return {
+        heading: navHeader,
+        items: navLinks,
+      };
+    }),
+    privacyLinks: [...privacyLinks].map((link) => ({
+      href: link.href,
+      name: link.innerText,
+    })),
+    socialLinks: [...socialLinks].map((link) => ({
+      href: link.href,
+      icon: link.querySelector('svg').outerHTML,
+    })),
+    copyrightText: copyRightText.innerText,
+    legalText: trademarksText.innerText,
+  };
+  block.innerHTML = ejs.render(getTemplate(), data);
+  block.classList.add('appear');
+}
+
+export async function decorate(block) {
   const container = document.createElement('div');
   container.classList.add('footer-container');
 
